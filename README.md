f2c44bd989c691b3a37612762e5c26cc7d096ae4
Noah Schneider <noah.schneider@outlook.com>
2021-04-05 17:54:10 +0200

    fixed zip creation

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Mod Dev Tools
+
 A simple toolset to make it easier to develop mods for factorio
 
+## Requirments
+
+Install 7zip
+
+```powershell
+choco install 7zip
+```
+
 ## Parameters
+
     [ValidateSet("test", "build")]
     [string] $action = "test",
     [Parameter()]
@@ -10,20 +20,21 @@ A simple toolset to make it easier to develop mods for factorio
     [string] $basePath = ".\",
     [Parameter()]
     [string] $zipPath = ".\zips"
-1. ```-action``` takes "test" for [development](##Development) or "build" to [generate a zip](##Publish). 
-1. ```-modIgnoreFile``` specifies where your .modignore files lies. It is good to have one of these to exclude stuff like .git and README.md from the zip if your mod uses source control.
-1. ```-basePath``` the path where you develop the mod
-1. ```-zipPath``` the path where the zip will be after running the script in build mode.
-  
+
+1. `-action` takes "test" for [development](##Development) or "build" to [generate a zip](##Publish).
+1. `-modIgnoreFile` specifies where your .modignore files lies. It is good to have one of these to exclude stuff like .git and README.md from the zip if your mod uses source control.
+1. `-basePath` the path where you develop the mod
+1. `-zipPath` the path where the zip will be after running the script in build mode.
+
 The default values only work if the script is copied into the mod folder itself.
 
 ## Development
 
-Run the script with ```-action "test"``` to test the mod.  
+Run the script with `-action "test"` to test the mod.  
 This will copy the folder into %appdata%\factorio\mods with the correct name.  
 The name is taken from info.json consists of name_version.
 
 ## Publish
 
-To publish simply run ```-action "build"```.  
+To publish simply run `-action "build"`.  
 This generates a zip file with the correct name which can be uploaded to the [modportal](https://mods.factorio.com).
--- mod_util.ps1
+++ mod_util.ps1
@@ -14,62 +14,75 @@ $basePath = Resolve-Path $basePath
 Write-Host "Using path:" $basePath
 
 $modIgnoreFile = $basePath + $modIgnoreFile
-if(!(Test-Path $modIgnoreFile)){
+if (!(Test-Path $modIgnoreFile)) {
     write-verbose "No modIgnoreFile found."
-}else{
+}
+else {
     $modIgnore = Get-Content $modIgnoreFile
 }
 
+$7zipPath = "$env:ProgramFiles\7-Zip\7z.exe"
+
+if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
+    throw "7 zip file '$7zipPath' not found"
+}
+
+Set-Alias 7zip $7zipPath
+
 $infoJsonPath = $basePath + ".\info.json"
-if(!(Test-Path $infoJsonPath)){
+if (!(Test-Path $infoJsonPath)) {
     write-error "No info.json found."
     exit
 }
 
 $info = Get-Content $infoJsonPath | ConvertFrom-Json
-$folderName=$info.name + "_" + $info.version
+$folderName = $info.name + "_" + $info.version
 write-host "ModFolderName" $folderName
 write-host "Factorio version" $info.factorio_version
 
-if($action -eq "test"){
+if ($action -eq "test") {
 
-    if($zipPath -ne ".\zips"){
+    if ($zipPath -ne ".\zips") {
         Write-Host "ZipPath is only used if the action is set to build" -ForegroundColor Red
     }
 
-    $path=[Environment]::GetFolderPath('ApplicationData') + "\Factorio\mods"
+    $path = [Environment]::GetFolderPath('ApplicationData') + "\Factorio\mods"
 
     write-host "Creating" $folderName "in" $path
 
-    if(!(Test-Path $path)){
+    if (!(Test-Path $path)) {
         New-Item -ItemType Directory -Force -Path $path
     }
 
     $modPath = $path + "\" + $folderName
 
-    if(Test-Path $modPath){
+    if (Test-Path $modPath) {
         Remove-Item $modPath"\*" -Recurse
-    }else{
+    }
+    else {
         New-Item -ItemType Directory -Force -Path $modPath
     }
+    Get-ChildItem $basePath | Where-Object { $_.Name -notin $modIgnore } | Copy-Item -Destination $modPath -Recurse -Force
 
-    Get-ChildItem $basePath | Where-Object{$_.Name -notin $modIgnore} | Copy-Item -Destination $modPath -Recurse -Force
-
-}elseif ($action -eq "build") {
+}
+elseif ($action -eq "build") {
     $zipPath = $zipPath + "\" + $folderName + ".zip"
-    if(Test-Path $zipPath){
+    if (Test-Path $zipPath) {
         Remove-Item $zipPath
     }
     $tempFolder = $env:TEMP + "\" + $folderName
-    if(Test-Path $tempFolder){
+    if (Test-Path $tempFolder) {
         Remove-Item $tempFolder"\*" -Recurse
-    }else{
+    }
+    else {
         New-Item -ItemType Directory -Force -Path $tempFolder
     }
-    Get-ChildItem $basePath | Where-Object{$_.Name -notin $modIgnore} | Copy-Item -Destination $tempFolder -Recurse -Force
-    Compress-Archive -Path $tempFolder -DestinationPath $zipPath -CompressionLevel "Fastest"
+    Get-ChildItem $basePath | Where-Object { $_.Name -notin $modIgnore } | Copy-Item -Destination $tempFolder -Recurse -Force
+    #Compress-Archive -Path $tempFolder -DestinationPath $zipPath -CompressionLevel "Fastest" #Does only work for windows (not modportal)
+    7zip a -mx=9 $zipPath $tempFolder
     Remove-Item $tempFolder -Recurse
 
-}else{
+}
+else {
     Write-Host "The action parameter was used incorrectly"
 }
